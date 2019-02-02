9c0a47e386593bc634ee5a2a2ce4c1351d338e68
Vinicius Albuquerque <viniciusalbuquerque@Viniciuss-MacBook-Pro.local>
2019-02-02 13:46:26 -0300

    Issue #1: Markdown Visualization
    Using react-markdown package to render Markdown texts.

---
+++ src/components/Post/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Post.css';
+import ReactMarkdown from 'react-markdown';
+
+class Post extends Component {
+    render() {
+	return (
+	    <div className="Post">
+		<ReactMarkdown source={this.props.marked_text} />
+	    </div>
+	);
+    }
+}
+
+export default Post;
