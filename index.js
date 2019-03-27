3d9de9afeb88abf821adf4e8c890ac8460c1fac2
Rodrigo Perazzo <rodrigo.perazzo@gmail.com>
2019-03-27 18:04:44 -0300

    Show files content on console
    First check if directory exists, creating it if not. Then clone or
    pull repository to get latest files. After that, show files content
    on console using git commands "ls-files", which list all files, and
    "show HEAD:<path-to-file>", which prints latest version of file
    regardless of which commit changed it last time.

--- index.js
+++ index.js
@@ -1,63 +1,68 @@
 const {app, BrowserWindow} = require('electron')
+const fs = require('fs')
 const REPO_PATH = 'git@github.com:DiscordTime/bloggit-mds.git';
 const LOCAL_PATH = 'lhebas'; // Define the local directory
 const gitP = require('simple-git/promise');
 const gitS = require('simple-git');
-const git = gitP();
+const git = gitP(LOCAL_PATH);
 
 let mainWindow
 
-function list_files() {
-//    gitP(REPO_PATH).raw(['ls-files'])
-//       .then(response => console.log('Raw data from git command: ', response))
-//       .catch(error => console.error(error));
-    gitS(LOCAL_PATH).raw(['ls-files'], (err, result) => {
-	if(err) {
-	    console.log(err)
-	    return
-	}
-
-	console.log('result', result)
-
-    })
+async function listFiles() {
+  return git.raw(['ls-files'])
 }
 
-function clone() {
-    console.log('index clone called')
-
-    console.log('git: clone called')
-	git.clone(REPO_PATH, LOCAL_PATH)
-	.then(result => {
-		console.log('result: ', result)
-		})
-    .catch(err => {
-	    console.log('error', err)
-	    })
+async function showFile(file) {
+  if (file != '') {
+    let content = await git.silent(true).show('HEAD:' + file)
+    console.log(content);
+  }
 }
 
+async function cloneOrPull() {
+  if (!fs.existsSync(LOCAL_PATH)){
+    console.log('LOCAL_PATH doesn\'t exist')
+    console.log('Creating folder');
+    fs.mkdirSync(LOCAL_PATH)
+    console.log('Calling git clone')
+    return git.clone(REPO_PATH);
+  } else {
+    console.log('Calling git pull')
+    return git.pull()
+  }
+}
 
-list_files()
+async function init() {
+  try {
+    await cloneOrPull()
+    let files = await listFiles()
+    files.split("\n").forEach(i => showFile(i));
+  } catch (e) {
+    console.log(e);
+  }
+}
+
 function createWindow () {
-    mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow = new BrowserWindow({width: 800, height: 600})
+  mainWindow.loadURL('http://localhost:3000')
 
-	mainWindow.loadURL('http://localhost:3000')
-
-	mainWindow.on('closed', function () {
-		mainWindow = null
-		})
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
 }
 
 app.on('ready', createWindow)
 
 app.on('window-all-closed', function () {
-	if (process.platform !== 'darwin') {
-	app.quit()
-	}
-	})
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
 
 app.on('activate', function () {
-	if (mainWindow === null) {
-	    createWindow()
-	}
-    })
+  if (mainWindow === null) {
+    createWindow()
+  }
+})
 
+init()
