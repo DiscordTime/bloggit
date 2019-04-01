a6a222ddd26f17c0bf2a6d10b7417207b74c0ee9
Victor Rattis <victorrattis@gmail.com>
2019-04-01 12:17:07 -0300

    Fix create git instance after creating the repo dir

--- main/git-command-bridge.js
+++ main/git-command-bridge.js
@@ -3,7 +3,6 @@ const fs = require('fs')
 const REPO_PATH = 'git@github.com:DiscordTime/bloggit-mds.git';
 const LOCAL_PATH = 'lhebas'; // Define the local directory
 const gitP = require('simple-git/promise');
-const git = gitP(LOCAL_PATH);
 const ACTION_LIST_MD_FILES = 'list-md-files'
 const ACTION_LIST_MD_FILES_DONE = 'list-md-files-done'
 
@@ -19,24 +18,30 @@ function throwException(message) {
 
 class GitCommandBridge {
 
+  setGitDir(dir) {
+    this.git = gitP(dir)
+  }
+
   async listFiles() {
-    return git.raw(['ls-files'])
+    return this.git.raw(['ls-files'])
   }
 
   async logFile(file) {
-    return git.silent(true).log({'file': file})
+    return this.git.silent(true).log({'file': file})
   }
 
   async showFile(file) {
-    return git.silent(true).show('HEAD:' + file)
+    return this.git.silent(true).show('HEAD:' + file)
   }
 
   async cloneOrPull() {
     if (!fs.existsSync(LOCAL_PATH)){
       fs.mkdirSync(LOCAL_PATH)
-      return git.clone(REPO_PATH);
+      this.setGitDir(LOCAL_PATH)
+      return this.git.clone(REPO_PATH, ".");
     } else {
-      return git.pull()
+      this.setGitDir(LOCAL_PATH)
+      return this.git.pull()
     }
   }
 
@@ -50,6 +55,7 @@ class GitCommandBridge {
             {markdown: values[0]},
             values[1].latest)
         })
+
   }
 
   async listMdFiles(event) {
