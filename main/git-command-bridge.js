04e9a792bb88b81bde9760166a3d5aacaae7cac9
Rodrigo Perazzo <rodrigo.perazzo@gmail.com>
2019-04-03 19:53:44 -0300

    Filter and sort markdowns
    Change git commands used to take into account commit order and only
    commits with markdown files. Also refactored initialization of git
    repo by checking/creating folder on class constructor.

--- main/git-command-bridge.js
+++ main/git-command-bridge.js
@@ -1,10 +1,11 @@
 const { ipcMain } = require('electron')
 const fs = require('fs')
 const REPO_PATH = 'git@github.com:DiscordTime/bloggit-mds.git';
-const LOCAL_PATH = 'lhebas'; // Define the local directory
+const LOCAL_PATH = 'repo'; // Define the local directory
 const gitP = require('simple-git/promise');
 const ACTION_LIST_MD_FILES = 'list-md-files'
 const ACTION_LIST_MD_FILES_DONE = 'list-md-files-done'
+const PAGE_SIZE = 10
 
 function throwIfIsNotMainProcess() {
     if (process && process.type !== 'browser') {
@@ -16,10 +17,35 @@ function throwException(message) {
     throw { name: "Exception", message: message }
 }
 
+String.prototype.splitGit = function(){
+  return this.split('\n').filter(value => value != '')
+}
+
 class GitCommandBridge {
+
+  constructor(){
+    if (!fs.existsSync(LOCAL_PATH)){
+      fs.mkdirSync(LOCAL_PATH)
+      fs.symlinkSync('../../' + LOCAL_PATH, 'src/res/repo')
+      this._shouldClone = true
+    }
+    this.setGitDir(LOCAL_PATH)
+    this._commitPointer = 'HEAD'
+  }
 
   setGitDir(dir) {
     this.git = gitP(dir)
+  }
+
+  async listNextCommitsWithMarkdownFiles(commitID) {
+    if (commitID != 'HEAD') {
+      commitID += '~1'
+    }
+    return this.git.raw(['rev-list',commitID,'--','\*md'])
+  }
+
+  async listMarkdownFilesOfCommit(commitID) {
+    return this.git.silent(true).show(['--pretty=', '--name-only', commitID,'--','\*md'])
   }
 
   async listFiles() {
@@ -35,12 +61,10 @@ class GitCommandBridge {
   }
 
   async cloneOrPull() {
-    if (!fs.existsSync(LOCAL_PATH)){
-      fs.mkdirSync(LOCAL_PATH)
-      this.setGitDir(LOCAL_PATH)
+    if (this._shouldClone){
+      this._shouldClone = false
       return this.git.clone(REPO_PATH, ".");
     } else {
-      this.setGitDir(LOCAL_PATH)
       return this.git.pull()
     }
   }
@@ -55,21 +79,29 @@ class GitCommandBridge {
             {markdown: values[0]},
             values[1].latest)
         })
+  }
 
+  mapCommitToFiles(commit) {
+    return this.listMarkdownFilesOfCommit(commit)
   }
 
   async listMdFiles(event) {
 
     try {
       await this.cloneOrPull()
-      let files = await this.listFiles()
-      let filesPromises = files.split("\n")
-        .filter(value => value != '')
-        .map(value => this.mapFilePathToFullObject(value))
 
-      let results = await Promise.all(filesPromises)
-      results.forEach(i => console.log(i))
-      event.sender.send(ACTION_LIST_MD_FILES_DONE, results)
+      let commits = await this.listNextCommitsWithMarkdownFiles(this._commitPointer)
+      commits = commits.splitGit()
+      for (let i = 0; i < Math.min(commits.length,PAGE_SIZE); i++) {
+        let commit = commits[i]
+        let files = await this.mapCommitToFiles(commit)
+        files = files.splitGit()
+        for (const file of files) {
+          let object = await this.mapFilePathToFullObject(file)
+          event.sender.send(ACTION_LIST_MD_FILES_DONE, object)
+        }
+        //this._commitPointer = commit
+      }
 
     } catch (e) {
       console.log(e);
